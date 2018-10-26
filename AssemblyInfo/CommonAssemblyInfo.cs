3f6b40a860bf25aa4d4b81faf5c7c78b33b0be41
NagiLin <johnny5581@gmail.com>
2018-10-26 17:04:10 +0800

    Rebuild solution and core project.

--- AssemblyInfo/CommonAssemblyInfo.cs
+++ AssemblyInfo/CommonAssemblyInfo.cs
@@ -9,4 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha")]
